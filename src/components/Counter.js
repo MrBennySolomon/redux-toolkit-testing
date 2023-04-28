0cdb3436af7678fd54f3c26c73e2220475e13232
MrBennySolomon <MrBennySolomon@gmail.com>
2023-04-28 20:54:50 +0300

    redux-toolkit-testing

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,8 @@
 import classes from './Counter.module.css';
+
+import { counterActions } from '../store/index';
 import { useSelector, useDispatch } from 'react-redux';
+
 const Counter = () => {
 
   const dispatch = useDispatch();
@@ -7,19 +10,19 @@ const Counter = () => {
   const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({type: 'increment'});
+    dispatch(counterActions.increment());
   }
 
   const increaseHandler = () => {
-    dispatch({type: 'increase', amount: 5});
+    dispatch(counterActions.increase(5)); //{ type: SOME_UNIQUE_IDENTIFIER, payload: 5 }
   };
 
   const decrementHandler = () => {
-    dispatch({type: 'decrement'});
+    dispatch(counterActions.decrement());
   }
 
   const toggleCounterHandler = () => {
-    dispatch({type: 'toggle'});
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
